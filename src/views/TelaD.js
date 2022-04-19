a541b709ed0d1a955700c835ced19191ce938a4a
Raizer Varela <raizer.varela@gmail.com>
2022-04-19 15:07:17 -0300

    Adição - Retorno do Tab e adição de icones

--- src/views/TelaD.js
+++ src/views/TelaD.js
@@ -12,7 +12,6 @@ export default props => {
             props.navigation.openDrawer()
             setTimeout(()=>{
               props.navigation.closeDrawer()
-
             }, 3000)
           }}  
         />
